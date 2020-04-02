c9724b9e25b2340cd65b51c45be720a87377ff7c
Javante Nelson <javantenelson@gmail.com>
2020-04-02 22:09:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name,artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def name
+    @name
+  end
+  
+  def artist
+    @artist
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    
+    
+  end
+  
+  def self.genres
+    @@genres = @@genres.unique
+  end
+  
+  def self.genre_count
+    @@genres = {}
+    @@genres.each do |genre|
+    if genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count  
+  end
+  
+  def self.artist_count
+  end
+end
