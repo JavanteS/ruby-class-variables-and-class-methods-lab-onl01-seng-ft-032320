f36f5180fb71799c6344eaeb1d0b16e61bedbab1
Javante Nelson <javantenelson@gmail.com>
2020-04-02 22:11:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,8 @@ class Song
   def self.genre_count
     @@genres = {}
     @@genres.each do |genre|
-    if genre_count[genre] += 1
+    if genre_count[genre]
+      genre_count[genre] += 1
     else
       genre_count[genre] = 1
     end
